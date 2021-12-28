937e6b828e0a161adf26400b9c976096cf82ee48
波吉 <512561580@qq.com>
2021-12-28 15:02:24 +0800

    43

---
+++ test_case/test_login.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+class TestGit():
+    def test_001(self):
+        print('ouououo')
+
+    def test_002(self):
+        print('ouououo')
+
+    def test_003(self):
+        print('ouououo')
+
+    def test_004(self):
+        print('ouououo')
+
+
+    def test_005(self):
+        print('ouououo')
+
+
+if __name__=="_main__":
+    pytest.main(['sv'])
